a000206368f89f5ac02c3d693b6f8286c81fa154
Ansalna <ansalnaksupm@gmail.com>
2025-11-22 14:50:48 +0530

    Files added

---
+++ src/Components/Greetings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Greetings() {
+  return (
+    <div>
+        <h2>CI/CD Introduction</h2>
+        <p>Welcome to all....!</p>
+    </div>
+  )
+}
+
+export default Greetings
